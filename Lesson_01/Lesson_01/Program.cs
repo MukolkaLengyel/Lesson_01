80f5bf715afd84481c4824adb6ac419a2b083f56
MukolkaLengyel <moonlight312004@gmail.com>
2023-03-02 22:39:07 +0100

    Lesson_01_Code

---
+++ Lesson_01/Lesson_01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("A one big man said: ");
+Console.WriteLine("Hello World!");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("Another said: ");
+Console.WriteLine("Diz Nats!");
+Console.ReadLine();
+//This is a comment <|0_0|>
